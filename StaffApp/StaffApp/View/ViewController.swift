1756f05213f58d7048653c33fe8eb6e0a3834b25
Elias Gabriel <elias.gabrielfn12@gmail.com>
2021-02-02 21:59:20 -0400

    xcode project added

---
+++ StaffApp/StaffApp/View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StaffApp
+//
+//  Created by Elias Gabriel dos Santos Correa on 02/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
